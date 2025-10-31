fe62c4d41f28ab1337469540ab7a642408926918
ohsung0722 <99doldol@naver.com>
2025-10-31 14:58:59 +0900

    feat(Lotto): 테스트 코드 설계를 기반으로 Lotto 클래스 추가 기능 구현
    숫자 배열 반환, 당첨 번호 매칭 갯수 판단, 숫자 포함 여부 판단하는 함수 추가

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -13,6 +13,23 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return [...this.#numbers];
+  }
+
+  //당첨 번호랑 몇개가 맞는지 return해주는 함수
+  matchCount(winningNumbers) {
+    const matchNumbers = this.#numbers.filter((number) =>
+      winningNumbers.includes(number)
+    );
+
+    return matchNumbers.length;
+  }
+
+  //입력한 숫자가 Number 객체에 있는지 판단하는 함수
+  contains(number) {
+    return this.#numbers.includes(number);
+  }
 }
 
 export default Lotto;
