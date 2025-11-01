088bce984b23cf1d11ccc3118e1235a8b88fbc7e
ohsung0722 <99doldol@naver.com>
2025-11-02 03:39:21 +0900

    feat(LottoService): 사용자 입력 금액에 따라 로또 객체를 생성하는 서비스 객체 구현

---
+++ src/service/LottoService.js
@@ -0,0 +1,35 @@
+import {
+  LOTTO_MAX_NUMBER,
+  LOTTO_MIN_NUMBER,
+  LOTTO_PRICE,
+  LOTTO_SIZE,
+} from "../constants/lotto.js";
+import Lotto from "../model/Lotto.js";
+import { generateSortedRandomNubmers } from "../utils/randomGenerator.js";
+
+class LottoService {
+  publish(amount) {
+    const count = this.#calculateLottoCount(amount);
+    const lottos = Array.from({ length: count }, () =>
+      this.#createSingleLotto()
+    );
+
+    return lottos;
+  }
+
+  #calculateLottoCount(amount) {
+    return Math.floor(amount / LOTTO_PRICE);
+  }
+
+  #createSingleLotto() {
+    const numbers = generateSortedRandomNubmers(
+      LOTTO_MIN_NUMBER,
+      LOTTO_MAX_NUMBER,
+      LOTTO_SIZE
+    );
+
+    return new Lotto(numbers);
+  }
+}
+
+export default LottoService;
