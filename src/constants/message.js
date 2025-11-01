ce6ac19b9f232e4863b736d66ca6bb2bab3ecc12
ohsung0722 <99doldol@naver.com>
2025-11-02 00:59:45 +0900

    feat(constant): 에러 메시지 상수화

--- src/constants/message.js
+++ src/constants/message.js
@@ -5,3 +5,11 @@ export const INPUT_MESSAGE = {
 };
 
 export const ERROR_PREFIX = (message) => `[ERROR] ${message}`;
+
+export const ERROR_MESSAGE = {
+  INVALID_PURCHASE_UNIT: "구입 금액은 1,000원 단위여야 합니다.",
+  INVALID_PURCHASE_MIN: "구입 금액은 1,000원 이상이어야 합니다.",
+  INVALID_LOTTO_NUMBER: "로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  INVALID_LOTTO_SIZE: "로또 번호는 6개여야 합니다.",
+  DUPLICATED_NUMBER: "로또 번호는 중복될 수 없습니다.",
+};
