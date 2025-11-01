be67b5468fe40e4826b21091962527653863d115
ohsung0722 <99doldol@naver.com>
2025-11-01 17:21:11 +0900

    feat(RankConfig): 게임 상세 규칙 명세된 config파일 생성

---
+++ src/config/RankConfig.js
@@ -0,0 +1,40 @@
+export const RANK_CONFIG = {
+  RANKS: [
+    {
+      name: "FIRST",
+      matchCount: 6,
+      bonus: false,
+      prize: 2000000000,
+      label: "6개 일치",
+    },
+    {
+      name: "SECOND",
+      matchCount: 5,
+      bonus: true,
+      prize: 30000000,
+      label: "5개 + 보너스 볼 일치",
+    },
+    {
+      name: "THIRD",
+      matchCount: 5,
+      bonus: false,
+      prize: 1500000,
+      label: "5개 일치",
+    },
+    {
+      name: "FOURTH",
+      matchCount: 4,
+      bonus: false,
+      prize: 50000,
+      label: "4개 일치",
+    },
+    {
+      name: "FIFTH",
+      matchCount: 3,
+      bonus: false,
+      prize: 5000,
+      label: "3개 일치",
+    },
+  ],
+  MISS: { name: "MISS", matchCount: 0, bonus: false, prize: 0, label: "" },
+};
