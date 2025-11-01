8f6665fa67955a6271d03983ff751d42e68607f4
ohsung0722 <99doldol@naver.com>
2025-11-02 03:00:41 +0900

    feat(validator): Input 값에 대한 검증 로직 추가

--- src/service/LottoValidator.js
+++ src/service/LottoValidator.js
@@ -1,7 +1,12 @@
-import { LOTTO_PRICE } from "../constants/lotto.js";
+import {
+  LOTTO_MAX_NUMBER,
+  LOTTO_MIN_NUMBER,
+  LOTTO_PRICE,
+  LOTTO_SIZE,
+} from "../constants/lotto.js";
 import { ERROR_MESSAGE } from "../constants/message.js";
 import DefaultError from "../error/DefaultError.js";
-import { isInteger } from "../utils/validator.js";
+import { hasNoDuplicates, isInRange, isInteger } from "../utils/validator.js";
 
 class LottoValidator {
   validatePurchaseAmount(amount) {
@@ -10,6 +15,27 @@ class LottoValidator {
     }
     if (amount < LOTTO_PRICE) {
       throw new DefaultError(ERROR_MESSAGE.INVALID_PURCHASE_MIN);
+    }
+  }
+
+  validateWinningNumbers(numbers) {
+    if (numbers.length !== LOTTO_SIZE) {
+      throw new DefaultError(ERROR_MESSAGE.INVALID_LOTTO_SIZE);
+    }
+
+    numbers.forEach((number) => this.validateLottoNumber(number));
+
+    if (!hasNoDuplicates(numbers)) {
+      throw new DefaultError(ERROR_MESSAGE.DUPLICATED_NUMBER);
+    }
+  }
+
+  validateLottoNumber(number) {
+    if (!isInteger(number)) {
+      throw new DefaultError(ERROR_MESSAGE.INVALID_LOTTO_NUMBER);
+    }
+    if (!isInRange(number, LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)) {
+      throw new DefaultError(ERROR_MESSAGE.INVALID_LOTTO_NUMBER);
     }
   }
 }
